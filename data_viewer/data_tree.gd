63d9eecf37a8711e3baff9346e33b3da9f42b5ce
Rabbit <rabbit@dfaction.net>
2019-05-31 03:40:24 -0700

    Added search icon & search button to json viewer
    Added a new search icon, as well as support for a search button in the json viewer. Button isn't yet operable, but it does display correctly.

--- data_viewer/data_tree.gd
+++ data_viewer/data_tree.gd
@@ -1,5 +1,7 @@
 extends Tree
 
+var _previous_selection: TreeItem
+
 
 func _on_SortOrder_expand_all_pressed():
 
@@ -9,4 +11,21 @@ func _on_SortOrder_expand_all_pressed():
 func _on_SortOrder_collapse_all_pressed():
 
 	TreeItemHelper.collapse_all_children(get_root(), false)
+
+
+func _on_DataTree_item_selected():
+
+	if _previous_selection:
+		_previous_selection.erase_button(1, 0)
+
+	get_selected().add_button(1, preload("res://data_viewer/search.png"), -1, false, "Search for matching entries")
+	_previous_selection = get_selected()
+
+
+func _on_DataTree_nothing_selected():
+
+	if _previous_selection:
+		_previous_selection.erase_button(1, 0)
+
+	_previous_selection = null
 
