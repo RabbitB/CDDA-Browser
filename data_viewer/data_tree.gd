0069ecbfde2146479ba6d4c77110dbb1de6f327d
Rabbit <rabbit@dfaction.net>
2019-05-31 03:08:51 -0700

    Update files to make use of new TreeItemHelper class
    Updates scripts to use TreeItemHelper where applicable.

--- data_viewer/data_tree.gd
+++ data_viewer/data_tree.gd
@@ -1,12 +1,12 @@
 extends Tree
 
 
-func _on_ExpandAllButton_pressed():
+func _on_SortOrder_expand_all_pressed():
 
 	TreeItemHelper.expand_all_children(get_root())
 
 
-func _on_CollapseAllButton_pressed():
+func _on_SortOrder_collapse_all_pressed():
 
 	TreeItemHelper.collapse_all_children(get_root(), false)
 
