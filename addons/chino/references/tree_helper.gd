debb4591cdfc2396ee3c9f25f95d0b1ce2610758
Rabbit <rabbit@dfaction.net>
2019-06-12 21:56:54 -0700

    Fix scrolling methods in TreeHelper
    Fixes the scrolling methods in TreeHelper to properly scroll in a predictable manner. Now nodes that are scrolled to, will appear as directed (at the top/bottom of the view, or "visible", which means the tree will scroll as it normally does.)

--- addons/chino/references/tree_helper.gd
+++ addons/chino/references/tree_helper.gd
@@ -1,23 +1,112 @@
 extends Reference
 class_name TreeHelper
 
+enum ScrollPlacement {
+	VISIBLE,
+	ON_TOP,
+	ON_BOTTOM
+}
 
-static func scroll_to_item(tree: Tree, item: TreeItem, select: bool = false, column: int = 0) -> void:
 
-	var item_is_selectable: bool = item.is_selectable(column)
-	var previously_selected_item: TreeItem = tree.get_selected()
-	var previously_selected_column: int = tree.get_selected_column()
+static func _scroll_to_item(tree: Tree, item: TreeItem, select: bool, column: int) -> void:
 
-	if !item_is_selectable:
-		item.set_selectable(column, true)
+	var scroll_selection: TreeSelection = TreeSelection.create(item, column)
+	var previous_selection: TreeSelection = TreeSelection.current_selection_of_tree(tree)
 
-	item.select(column)
+	var y = scroll_selection.force_temp_select()
 	tree.ensure_cursor_is_visible()
+	y.resume()
 
-	if !item_is_selectable:
-		item.set_selectable(column, false)
+	if select:
+		scroll_selection.select()
+	elif previous_selection.item != null:
+		previous_selection.select()
 
-	if !select && previously_selected_item != null:
+
+static func scroll_to_item(
+		tree: Tree,
+		item: TreeItem,
+		select: bool = false,
+		column: int = 0,
+		placement: int = ScrollPlacement.VISIBLE) -> void:
+
+	if placement == ScrollPlacement.ON_TOP:
+		scroll_to_bottom(tree)
+	elif placement == ScrollPlacement.ON_BOTTOM:
+		scroll_to_top(tree)
+
+	_scroll_to_item(tree, item, select, column)
+
+
+static func scroll_to_top(tree: Tree) -> void:
+
+	var scroll_bar: VScrollBar = NodeHelper.get_child_of_class(tree, "VScrollBar") as VScrollBar
+	scroll_bar.value = scroll_bar.min_value
+
+
+static func scroll_to_bottom(tree: Tree) -> void:
+
+	var scroll_bar: VScrollBar = NodeHelper.get_child_of_class(tree, "VScrollBar") as VScrollBar
+	scroll_bar.value = scroll_bar.max_value
+
+
+class TreeSelection:
+
+	var item: TreeItem
+	var column: int
+
+
+	static func create(item: TreeItem, column: int) -> TreeSelection:
+
+		var new_selection: TreeSelection = TreeSelection.new()
+
+		new_selection.item = item
+		new_selection.column = column
+
+		return new_selection
+
+
+	static func current_selection_of_tree(tree: Tree) -> TreeSelection:
+
+		if tree:
+			return create(tree.get_selected(), tree.get_selected_column())
+
+		return TreeSelection.new()
+
+
+	func select() -> void:
+
+		if !item:
+			return
+
+		item.select(column)
+
+
+	func force_select() -> void:
+
+		if !item:
+			return
+
+		if !item.is_selectable(column):
+			item.set_selectable(column, true)
+
+		item.select(column)
+
+
+	func force_temp_select() -> void:
+
+		if !item:
+			return
+
+		var item_is_selectable: bool = item.is_selectable(column)
+
+		if !item_is_selectable:
+			item.set_selectable(column, true)
+
+		item.select(column)
+		yield()
 		item.deselect(column)
-		previously_selected_item.select(previously_selected_column)
+
+		if !item_is_selectable:
+			item.set_selectable(column, false)
 
